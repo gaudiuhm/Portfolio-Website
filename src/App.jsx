a0bdd50bc63a16aadeadc0b81b9377b19d237511
Giulian Giannone <giuliangiannone7@gmail.com>
2024-02-24 01:06:17 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/9sf3r1f4zGt3Evlu/scene.splinecode" />
+  );
+}
