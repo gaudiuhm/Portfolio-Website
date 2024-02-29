ee2ff7f89f637739f1d629c18d27a039c6f992f3
Giulian Giannone <giuliangiannone7@gmail.com>
2024-02-28 22:48:28 -0500

    Fixed sign interactions, more direction on where to go

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import Spline from '@splinetool/react-spline';
+import React, { Suspense } from 'react';
+
+const Spline = React.lazy(() => import('@splinetool/react-spline'));
 
 export default function App() {
   return (
-    <Spline scene="https://prod.spline.design/9sf3r1f4zGt3Evlu/scene.splinecode" />
+    <div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Spline scene="https://prod.spline.design/9sf3r1f4zGt3Evlu/scene.splinecode" />
+      </Suspense>
+    </div>
   );
 }
